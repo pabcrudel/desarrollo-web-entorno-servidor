e4e5c03800271b32c3f2aa63d0bc6f4f85e4deb0
pabcrudel <pabcrudel@gmail.com>
2023-10-21 18:38:53 +0200

    Create some constant variables

---
+++ variables/constantes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Variables constantes</title>
+</head>
+<body>
+  <a href="../">Home</a>
+
+  <h1>Uso de variables constantes</h1>
+
+  <?php
+    define("PI", 3.14159);
+  ?>
+
+  <p> <?php echo PI; ?> </p>
+
+  <a href="/">Uso de variables</a>
+</body>
+</html>
