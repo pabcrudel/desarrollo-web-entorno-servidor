253a824d65bf3d8fea2a3dedc156c9359782ee87
pabcrudel <pabcrudel@gmail.com>
2023-10-21 18:27:33 +0200

    Example of how to use variables

---
+++ variables/index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Variables</title>
+</head>
+<body>
+  <h1>Uso de variables</h1>
+
+  <?php
+    $hora = 12;
+    $minutos = 15;
+    $ciudad = "Sevilla";
+    $texto = "El tren destino $ciudad sale a las $hora:$minutos"
+  ?>
+
+  <p> <?=$texto;?> </p>
+</body>
+</html>
